3bb3b2309fc7435f5e308bedbe5807556f4cfb9f
Samuelkoeanteng <korantengsamuel1@gmail.com>
2023-03-16 20:17:30 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point for puts function task
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
