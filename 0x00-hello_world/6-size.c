86c185d5b19cf7e8d957f3959bda46f56249d49a
Samuelkoeanteng <korantengsamuel1@gmail.com>
2023-03-17 04:58:19 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point for Size is not grandeur and territory does not make a nation
+ * main - Entry point for Size and territory task
  * Return: 0
  */
 int main(void)
 {
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long) sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
 return (0);
 }
