b4e7a153e7ea9d04d4f04ea2201a754d62e975ac
Samuelkoeanteng <korantengsamuel1@gmail.com>
2023-03-23 15:49:15 -0700

    Multiple main

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-My first debugging readme
+0x03-debugging
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,11 +3,11 @@
 void positive_or_negative(int i)
 {
 	if (i > 0)
-		printf("%i is positive\n", i);
-
+		printf("%d is positive\n", i);
+	
 	else if (i < 0)
-		printf("%i is negative\n", i);
+		printf("%d is negative\n", i);
 
-	else
-                printf("%i is zero\n", i);
+	else 
+		printf("%d is zero\n" , i);
 }
