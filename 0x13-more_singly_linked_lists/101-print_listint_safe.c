368a6d4a45ce33bc51072e4a2f78384ee2960b32
Samuelkoeanteng <korantengsamuel1@gmail.com>
2023-05-02 17:22:56 -0700

    print_listint_safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  * print_listint_safe -  a function that prints
+  * a listint_t linked list.
+  * @head: head of the link list
+  * Return:  the number of nodes in the list
+  * If the function fails, exit the program with status 98
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
+
